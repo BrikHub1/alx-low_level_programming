d39fe4a3e0ed084891005da25b60c25a00385df3
BrikHub1 <brikamalou@gmail.com>
2023-03-17 10:09:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main -  A program that print a line using the printf function
- * Return: 0 (success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+puts("\"Programming is like building a multilingual puzzle
+");
 return (0);
-}	
+}
