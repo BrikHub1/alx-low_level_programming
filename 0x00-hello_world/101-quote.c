069122ea12611b1f59f0f96e8829dc358772c21a
BrikHub1 <brikamalou@gmail.com>
2023-03-20 10:07:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- *  main - Entry point
+ * main - Entry point
  * Return: 1 (if Successful)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora kopar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
